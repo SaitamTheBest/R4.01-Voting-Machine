563fb4696dd92f91357dd3e724401bad9a8724f8
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2024-01-26 17:23:02 +0100

    ajout de fonctionnalité pour les votes (a finir)

--- v010_spaghetti/src/main.rs
+++ v010_spaghetti/src/main.rs
@@ -1,20 +1,42 @@
+
+
 fn main() -> anyhow::Result<()>{
     use std::io::{self, BufRead};
+
+    let mut votants : Vec<&str> = vec![];
+    let mut scores : Vec<&str> = vec![];
 
     let stdin = io::stdin();
     let lines = stdin.lock().lines();
 
+    println!("Commandes valides :");
+    println!("- \"voter Tux NixOS\" : Tux vote pour NixOS");
+    println!("- \"voter Tux\" : Tux vote blanc");
+    println!("- \"votants\" : affiche la liste des votants");
+    println!("- \"scores\" : affiche les scores pour tous les candidats");
+
     for line in lines {
         match line {
             Ok(line_content) => {
-                match line_content.as_str() {
+                let mut vector = vec![];
+                vector = line_content.as_str().split_whitespace().collect();
+                match vector[0] {
                     "voter" => {
+                        if vector.len()==2 {
+                            votants.push(vector[1]);
+                            scores.push(vector[2]);
+                        }
+                        else {
+                            votants.push(vector[1]);
+                        }
                         println!("Commande trouvé");
                     }
                     "votants" => {
+                        println!("{:?}",votants);
                         println!("Commande trouvé");
                     }
                     "scores" => {
+                        println!("{:?}",scores);
                         println!("Commande trouvé");
                     }
                     _ => {
